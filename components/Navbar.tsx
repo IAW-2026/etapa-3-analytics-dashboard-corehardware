a308cf7e92db3f9019cf4e11065d9f20b3839293
AgusF <agus_p3@hotmail.com>
2026-06-25 17:27:26 -0300

    fixed broken import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { UserButton, SignedIn } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 
